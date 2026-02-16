96ec3720bfac59b53a95cc5b2dea4778c17163e0
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2026-02-16 17:59:54 +0900

    update-08　再

--- js/include.js
+++ js/include.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('site-header').innerHTML = html;
     });
 
-  // fetch('/parts/contact.html')
-  //   .then(res => res.text())
-  //   .then(html => {
-  //     document.getElementById('contact').innerHTML = html;
-  //   });
+  fetch('/parts/contact.html')
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('contact').innerHTML = html;
+    });
 
   fetch('parts/footer.html')
     .then(res => res.text())
